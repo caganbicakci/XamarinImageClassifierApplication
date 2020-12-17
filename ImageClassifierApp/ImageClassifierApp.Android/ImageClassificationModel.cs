27ffcb48335b915abfcc07088cb1c5f6219f6b10
Çayan Bıçakçı <44499663+caganbicakci@users.noreply.github.com>
2020-12-17 23:48:47 +0300

    ImageClassifierApp

---
+++ ImageClassifierApp/ImageClassifierApp.Android/ImageClassificationModel.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageClassifierApp.Droid
+{
+    public class ImageClassificationModel
+    {
+        public ImageClassificationModel(string tagName, float probability)
+        {
+            TagName = tagName;
+            Probability = probability;
+        }
+
+        public float Probability { get; }
+        public string TagName { get; }
+
+    }
+}
---
+++ ImageClassifierApp/ImageClassifierApp/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xam.Plugins.OnDeviceCustomVision;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Essentials;
+using System.IO;
+
+
+namespace ImageClassifierApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //TakePhotoBtn.Clicked += async (sender, args) =>
+            //{
+            //    if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            //    {
+            //        await DisplayAlert("No Camera", ":( No camera available.", "OK");
+            //        return;
+            //    }
+
+            //    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            //    {
+            //        Directory = "Test",
+            //        SaveToAlbum = true,
+            //        CompressionQuality = 75,
+            //        CustomPhotoSize = 50,
+            //        PhotoSize = PhotoSize.MaxWidthHeight,
+            //        MaxWidthHeight = 2000,
+            //        DefaultCamera = CameraDevice.Front
+            //    });
+
+            //    if (file == null)
+            //        return;
+
+            //    Image.Source = ImageSource.FromStream(() =>
+            //    {
+            //    var stream = file.GetStream();
+            //    file.Dispose();
+            //    return stream;
+            //    });
+            //};
+
+
+
+        }
+
+
+
+    }
+
+
+    
+}
